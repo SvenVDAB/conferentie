72aad746b709a0ee14baf6f8657e0d6cddd53caf
SvenBouwen <sven.bouwen@vdabcampus.be>
2022-12-05 12:31:34 +0100

    Initial commit

---
+++ src/main/java/be/vdab/conferentie/ConferentieApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.conferentie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferentieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferentieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/vdab/conferentie/ConferentieApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.conferentie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferentieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
